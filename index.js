a9095a89c46fd19739586f00965d476bff3a0e05
Harun Akgul <46harunakgul@gmail.com>
2022-09-03 16:17:32 +0300

    Some Attributes Adding js file.

--- index.js
+++ index.js
@@ -25,6 +25,42 @@ $(document).keypress(function(event){
 });
 */
 
+/*
 $("h1").on("mouseover", function(){
     $("h1").css("color", "purple")
+});
+*/
+/*
+$("h1").on("click", function(){
+    $("h1").hide();
+});
+*/
+/*
+$("h1").on("click", function(){
+    $("h1").fadeOut();
+});
+*/
+
+/*
+$("h1").on("click", function(){
+    $("h1").toggle();
+});
+*/
+/*
+$("h1").on("click", function(){
+    $("h1").slideUp();
+});
+*/
+/*
+$("h1").on("click", function(){
+    $("h1").slideToggle();
+});
+*/
+/*
+$("h1").on("click", function(){
+    $("h1").animate({opacity : 0.5, margin : 20})
+});
+*/
+$("h1").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity : 0.5})
 });
