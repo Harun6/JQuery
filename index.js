16e27011a1fc4fbf22f659adc0ea8324c803eef8
Harun Akgul <46harunakgul@gmail.com>
2022-09-03 15:58:46 +0300

    Adding New Attributes JQuery

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 
-$("h1").text("Hey");
+//$("h1").text("Hey");
 
-$("button").html("<em>Don't Click Me</em>")
+//$("button").html("<em>Don't Click Me</em>")
+
+
+//$("a").attr("href", "https://www.yahoo.com");
+
+/* for(let i = 0; i < 5; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        document.querySelector("h1").style.color = "purple";
+    });
+}
+*/
+/*
+
+$("button").click(function(){
+    $("h1").css("color", "purple");
+});
+
+*/
+/*
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+});
+*/
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "purple")
+});
